d521ca4ecbb1b2d5ee60cd55b11f9315a6708b9b
kapui_niki <kapui_niki@sch.bme.hu>
2023-03-10 11:11:27 +0100

    API fix maybe done

--- server/api.js
+++ server/api.js
@@ -64,7 +64,7 @@ app.get('/products/search', async (request, response) => {
   if(fav != undefined) {
     fav = fav.split(",");
     fav = fav.map(id => ObjectId(id))
-    
+
     find["_id"] = {$in: fav};
   }
 
@@ -110,11 +110,11 @@ app.get('/products/search', async (request, response) => {
 
     const temp_p_value = [...result].sort((A_prod, B_prod) => (B_prod.price - A_prod.price));
     const p50_index = Math.floor(temp_p_value.length * 0.5);
-    end_result["data"]["meta"]["p50"] = temp_p_value[p50_index].price;;
+    end_result["data"]["meta"]["p50"] = temp_p_value[p50_index].price;
     const p90_index = Math.floor(temp_p_value.length * 0.9);
-    end_result["data"]["meta"]["p90"] = temp_p_value[p90_index].price;;
+    end_result["data"]["meta"]["p90"] = temp_p_value[p90_index].price;
     const p95_index = Math.floor(temp_p_value.length * 0.95);
-    end_result["data"]["meta"]["p95"] = temp_p_value[p95_index].price;;
+    end_result["data"]["meta"]["p95"] = temp_p_value[p95_index].price;
 
     end_result["data"]["meta"]["lastDate"] = [...result].sort((A_prod, B_prod) => (new Date(B_prod.released) - new Date(A_prod.released)))[0].scrape_date;
     end_result["data"]["result"] = result.slice((parseInt(page) - 1) * parseInt(limit), parseInt(page) * parseInt(limit));
@@ -122,7 +122,20 @@ app.get('/products/search', async (request, response) => {
   }
   catch{
     end_result["success"] = false;
-    end_result["data"] = "Invalid parameters";
+    end_result["data"]["meta"]["count"] = 0;
+    end_result["data"]["meta"]["currentPage"] = 1;
+    end_result["data"]["meta"]["pageCount"] = 1;
+    end_result["data"]["meta"]["pageSize"] = parseInt(limit);
+
+    end_result["data"]["meta"]["newCount"] = 0;
+
+    end_result["data"]["meta"]["p50"] = 0;
+    end_result["data"]["meta"]["p90"] = 0;
+    end_result["data"]["meta"]["p95"] = 0;
+
+    end_result["data"]["meta"]["lastDate"] = new Date();
+    end_result["data"]["result"] = [];
+    
     response.send(end_result);
   }
 });
